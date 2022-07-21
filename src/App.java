280e00ac35ead60a35191eb378d10e2d6bd74d8a
William <williancrkw@gmail.com>
2022-07-20 21:40:49 -0300

    Atualização da segunda aula

--- src/App.java
+++ src/App.java
@@ -1,13 +1,18 @@
 package src;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.*;
 import java.util.List;
 import java.util.Map;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+
+// Montagem do Http e conexão
         String url = "https://imdb-api.com/en/API/Top250TVs/k_2djg2apy";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -15,17 +20,27 @@ public class App {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
 
+
 // extrair só os dados que interessam ( titulo, poster, classificação
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
 
 // exibir e manipular os dados
-
+        var geradora = new GeradorDeSticker();
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
+
         }
 
     }
---
+++ src/GeradorDeSticker.java
@@ -0,0 +1,40 @@
+package src;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GeradorDeSticker {
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws IOException {
+
+
+        // leitura da imagem
+        BufferedImage imagemOrinal = ImageIO.read(new File("/C:/Users/crkw/OneDrive/Imagens/filme.jpg"));
+
+        // cria nova imagem em memoria com transparencia e com o tamanho novo
+        int largura = imagemOrinal.getWidth();
+        int altura = imagemOrinal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original pra nova imagem ( em memoria)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOrinal, 0, 0,null);
+
+        // configurar a fonte
+        var fonte =  new Font(Font.SANS_SERIF, Font.BOLD, 60);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("DAHORA!",100,novaAltura - 100);
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File("/C:/Users/crkw/OneDrive/Imagens/Desligamento/filme.png"));
+
+    }
+}
