c407f722830f5eb397310281ee2c7f6d7ce3018b
William <williancrkw@gmail.com>
2022-07-24 19:24:28 -0300

    Atualização aula 5.

--- linguagens-api/linguagens-api/src/main/java/br/com/alura/imersao/linguagensapi/Linguagem.java
+++ linguagens-api/linguagens-api/src/main/java/br/com/alura/imersao/linguagensapi/Linguagem.java
@@ -1,16 +1,18 @@
 package br.com.alura.imersao.linguagensapi;
 
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Document(collection = "principaisLinguagens")
 public class Linguagem {
 
+    @Id
     private String id;
 
     private String title;
-    private String imagem;
+    private String image;
     private int ranking;
 
     public Linguagem () {
@@ -18,10 +20,10 @@ public class Linguagem {
     }
 
 
-    public Linguagem(String id, String title, String imagem, int ranking) {
+    public Linguagem(String id, String title, String image, int ranking) {
         this.id = id;
         this.title = title;
-        this.imagem = imagem;
+        this.image = image;
         this.ranking = ranking;
     }
 
